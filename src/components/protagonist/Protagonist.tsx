c2adc0a768ec2a265c2e9581749d5f2ea9c54aba
Sahar Soffer <ssoffer@medallia.com>
2021-09-12 20:08:39 +0300

    move coordinates creator to customhooks

--- src/components/protagonist/Protagonist.tsx
+++ src/components/protagonist/Protagonist.tsx
@@ -1,46 +1,25 @@
 import React, {useEffect, useRef, useState} from 'react';
 import ConstantsEnum from '../../models/enum.constants';
 import './Protagonist.scss';
+import useProtagonistMovement from './hooks/useProtagonistMovement';
 
 export type IProtagonistProps = {
     arena: HTMLElement;
 }
 
-
-
 const Protagonist: React.FunctionComponent<IProtagonistProps> = ({arena}) => {
 
     const protagonistRef = useRef(null);
 
-    const [left, setLeft] = useState<number>(0);
-    const [top, setTop] = useState<number>(0);
-
-    function onMouseMove(e: MouseEvent) {
-        if(!protagonistRef.current) return;
-        const protagonistWidth = protagonistRef.current.clientWidth;
-        const protagonistHeight = protagonistRef.current.clientWidth;   
-        const x = e.clientX - protagonistWidth / 2;
-        const y = e.clientY - protagonistHeight / 2;
-        setLeft(x);
-        setTop(y);
-    }
-
-    useEffect(()=>{
-        if(!arena) return;
-        window.addEventListener('mousemove', onMouseMove);
-        return function(){
-            window.removeEventListener('mousemove', onMouseMove);
-        };
-    }, [arena]);
-
+    const coordinates = useProtagonistMovement(protagonistRef);
 
     return <div 
         ref={protagonistRef}
         id={ConstantsEnum.PROTAGONIST_ID} 
         className="sis-protagonist"
         style={{
-            top: `${top}px`,
-            left: `${left}px`,
+            top: `${coordinates.top}px`,
+            left: `${coordinates.left}px`,
         }}
     ></div>;
 };
---
+++ src/components/protagonist/hooks/useProtagonistMovement.ts
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+
+export default function useProtagonistMovement(protagonistRef){
+    
+    const [left, setLeft] = useState<number>(0);
+    const [top, setTop] = useState<number>(0);
+
+    function onMouseMove(e: MouseEvent) {
+        if(!protagonistRef.current) return;
+        const protagonistWidth = protagonistRef.current.clientWidth;
+        const protagonistHeight = protagonistRef.current.clientWidth;   
+        const x = e.clientX - protagonistWidth / 2;
+        const y = e.clientY - protagonistHeight / 2;
+        setLeft(x);
+        setTop(y);
+    }
+
+    useEffect(()=>{
+        if(!protagonistRef.current) return;
+        window.addEventListener('mousemove', onMouseMove);
+        return function(){
+            window.removeEventListener('mousemove', onMouseMove);
+        };
+    }, []);
+
+    return {top, left};
+
+}
