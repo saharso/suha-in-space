0c1afc007513923232f82db24a2e2a4da9f56cdf
Sahar Soffer <ssoffer@medallia.com>
2021-09-12 19:59:46 +0300

    build protagonist mobility on mouse move

--- src/components/protagonist/Protagonist.tsx
+++ src/components/protagonist/Protagonist.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import ConstantsEnum from '../../models/enum.constants';
 import './Protagonist.scss';
 
@@ -6,15 +6,43 @@ export type IProtagonistProps = {
     arena: HTMLElement;
 }
 
+
+
 const Protagonist: React.FunctionComponent<IProtagonistProps> = ({arena}) => {
+
+    const protagonistRef = useRef(null);
+
+    const [left, setLeft] = useState<number>(0);
+    const [top, setTop] = useState<number>(0);
+
+    function onMouseMove(e: MouseEvent) {
+        if(!protagonistRef.current) return;
+        const protagonistWidth = protagonistRef.current.clientWidth;
+        const protagonistHeight = protagonistRef.current.clientWidth;   
+        const x = e.clientX - protagonistWidth / 2;
+        const y = e.clientY - protagonistHeight / 2;
+        setLeft(x);
+        setTop(y);
+    }
 
     useEffect(()=>{
         if(!arena) return;
-        console.log(arena);
+        window.addEventListener('mousemove', onMouseMove);
+        return function(){
+            window.removeEventListener('mousemove', onMouseMove);
+        };
     }, [arena]);
 
 
-    return <div id={ConstantsEnum.PROTAGONIST_ID} className="sis-protagonist"></div>;
+    return <div 
+        ref={protagonistRef}
+        id={ConstantsEnum.PROTAGONIST_ID} 
+        className="sis-protagonist"
+        style={{
+            top: `${top}px`,
+            left: `${left}px`,
+        }}
+    ></div>;
 };
 
 export default Protagonist;
