14b4c0a6d661367bf2b08c69c3d005fbf8a4276c
Sahar Soffer <ssoffer@medallia.com>
2021-09-16 18:21:13 +0300

    build onProtagonistHit callback

--- src/components/enemies/hooks/useGenerateEnemies.ts
+++ src/components/enemies/hooks/useGenerateEnemies.ts
@@ -3,20 +3,23 @@ import { isConditionalExpression } from 'typescript';
 import AppContext from '../../../models/context';
 import ConstantsEnum from '../../../models/enum.constants';
 
-const rate = 1000;
+function getProtagonist(){
+    return document.getElementById(ConstantsEnum.PROTAGONIST_ID);
+}
 
-function wasHit(mutation, enemyRef){
-    if(mutation.target.id === ConstantsEnum.PROTAGONIST_ID) return;
-    const activeBUllet: HTMLElement = mutation.target;
-    const enemy: HTMLElement = enemyRef;
-    const bulletLeftPos = activeBUllet.getBoundingClientRect().left;
-    const bulletRightPos = activeBUllet.getBoundingClientRect().right;
-    const enemyLeftPos = enemy.getBoundingClientRect().left;
-    const enemyRightPos = enemy.getBoundingClientRect().right;
+function elementsOverlap(el1: HTMLElement, el2: HTMLElement): boolean {
 
-    const hit = bulletRightPos > enemyLeftPos && bulletLeftPos < enemyRightPos;
+    const rect1 = el1.getBoundingClientRect();
+    const rect2 = el2.getBoundingClientRect();
 
-    return hit;
+    const overlap = !(
+
+        rect1.right < rect2.left || 
+        rect1.left > rect2.right || 
+        rect1.bottom < rect2.top || 
+        rect1.top > rect2.bottom
+    );
+    return overlap;
 }
 
 function observeEnemyBulletRelations(protagonistEl, enemyRef, callback?: Function){
@@ -25,16 +28,21 @@ function observeEnemyBulletRelations(protagonistEl, enemyRef, callback?: Functio
 
     const config = { attributes: true, childList: true, subtree: true };
 
+    const protagonist = getProtagonist();
+
     const onMutation = function(mutationsList, observer) {
 
         for(const mutation of mutationsList) {
 
             if (mutation.type === 'attributes') {
 
-                const enemies = enemyRef.children;
+                const enemies: NodeList = enemyRef.children;
 
-                enemies && Array.from(enemies).forEach(enemyItem => {
-                    if(wasHit(mutation, enemyItem)) {
+                enemies && Array.from(enemies).forEach((enemyItem: HTMLElement) => {
+                    if(elementsOverlap(protagonist, enemyItem)){
+                        callback && callback();
+                    } 
+                    if (elementsOverlap(mutation.target, enemyItem)) {
                         removeElement(enemyItem);
                     }
                 });
@@ -81,10 +89,12 @@ function moveDownwards(enemy, leaveAfterMs){
 interface IEnemyConfig {
     firingRate: number;
     speed: number;
+    onProtagonistHit: Function;
 }
 class Enemy implements IEnemyConfig {
     firingRate = 1000;
     speed = 2000;
+    onProtagonistHit: Function;
     private interval;
     private observer;
     private enemyOrigin;
@@ -106,8 +116,8 @@ class Enemy implements IEnemyConfig {
         this.virtualHolder.appendChild(this.enemyOrigin);
         this.interval = setInterval(()=>{
             this.generateEnemies(this.enemyWrapper, this.enemyOrigin);
-        }, rate);
-        this.observer = observeEnemyBulletRelations(this.protagonistEl, this.enemyWrapper);
+        }, this.firingRate);
+        this.observer = observeEnemyBulletRelations(this.protagonistEl, this.enemyWrapper, this.onProtagonistHit);
     }
 
     public kill(){
@@ -132,6 +142,10 @@ export default function useGenerateEnemies(ref) {
 
         const enemy = new Enemy(ref.current, protagonistEl, {
             speed: 2000,
+            firingRate: 3000,
+            onProtagonistHit: ()=>{
+                console.log('hit');
+            }
         });
 
         return function(){
