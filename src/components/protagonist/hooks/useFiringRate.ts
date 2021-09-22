2338a7a776aab1157c0807db000829b517229953
Sahar Soffer <saharsoffer@Sahars-MacBook-Pro.local>
2021-09-22 10:50:51 +0300

    refactor generation with BulletModel

--- src/components/protagonist/hooks/useFiringRate.ts
+++ src/components/protagonist/hooks/useFiringRate.ts
@@ -1,9 +1,37 @@
 import React, {useState, useEffect, useContext, useCallback} from 'react';
 import ProtagonistContext from '../models/protagonistContext';
+import overrideClassPropsTool from '../../../global/tools/overrideClassProps.tool';
+
+class BulletConfig {
+    speed: number = 200;
+    generationRate: number = 500;
+    constructor (config?: Partial<BulletConfig>){
+        overrideClassPropsTool.call(this, config);
+    }
+}
+
+class BulletModel extends BulletConfig {
+    private bulletModel = document.createElement('div');
+    constructor(config?: Partial<BulletConfig>) {
+        super(config);
+        this.buildBulletModel();
+    }
+
+    private buildBulletModel () {
+        this.bulletModel.className = 'sis-singleDefaultBullet';
+    }
+
+}
+
+const config = new BulletConfig({
+    generationRate: 500,
+});
+
+console.log(config);
 
 export default function useFiringRate(parentRef, coordinates) {
-
-    const config = useContext(ProtagonistContext);
+    
+    const [bulletModel, setBulletModel] = useState(null);
 
     const [bullets, setBullets] = useState([]);
     
@@ -17,9 +45,11 @@ export default function useFiringRate(parentRef, coordinates) {
 
     useEffect(()=>{
 
+        setBulletModel(new BulletModel(config));
+
         const interval = setInterval(()=>{
             setBullets(prev => {prev.push(null); return [...prev];});
-        }, config.firingRate);
+        }, config.generationRate);
 
         return function(){
             clearInterval(interval);
