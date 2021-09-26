4185fafe7a0df85c04d18eee38e3be69639d63bd
Sahar Soffer <saharsoffer@Sahars-MacBook-Pro.local>
2021-09-26 06:27:08 +0300

    add a strength method to BulletConfig

--- src/components/protagonist/models/BulletConfig.model.ts
+++ src/components/protagonist/models/BulletConfig.model.ts
@@ -1,11 +1,13 @@
 import overrideClassPropsTool from '../../../global/tools/overrideClassProps.tool';
 import GlobalConfig from '../../../global/models/MGlobalConfig';
+import ICoordinates from './iCoordinates';
 
 export default class BulletConfig extends GlobalConfig {
     speed: number = 300;
     generationRate: number = 40;
     parentEl: HTMLElement;
-    coordinates;
+    coordinates: ICoordinates;
+    strength: number = 1;
     constructor (config?: Partial<BulletConfig>){
         super();
         this.update(config);
